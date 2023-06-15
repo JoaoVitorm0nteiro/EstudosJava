f638107ab42e846e31411a5f55cb0d5100a02e7d
JoaoVitorm0nteiro <jv147pao@gmail.com>
2023-06-15 14:27:16 -0300

    Aprimoraramento do exercicio.

---
+++ Carro_teste/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        carro  BMW = new carro("BMW", "BMW M3", 2015);
+        BMW.start();
+        BMW.acelerar();
+
+    }
+}
